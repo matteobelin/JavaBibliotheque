eda064a7e5e08607ba4ac8b6e7e576101ddc0370
matteobelin <matteobelin85@gmail.com>
2025-03-06 18:30:45 +0100

    feat:add an author

--- src/main/java/com/esgi/data/authors/impl/AuthorRepositoryImpl.java
+++ src/main/java/com/esgi/data/authors/impl/AuthorRepositoryImpl.java
@@ -1,12 +1,17 @@
 package com.esgi.data.authors.impl;
 
+import com.esgi.core.exceptions.ConstraintViolationException;
+import com.esgi.core.exceptions.helpers.SQLExceptionEnum;
+import com.esgi.core.exceptions.helpers.SQLExceptionParser;
 import com.esgi.data.Repository;
 import com.esgi.data.authors.AuthorModel;
 import com.esgi.data.authors.AuthorRepository;
-import jdk.jshell.spi.ExecutionControl;
 
+
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 
 public class AuthorRepositoryImpl extends Repository<AuthorModel> implements AuthorRepository {
     @Override
@@ -24,8 +29,30 @@ public class AuthorRepositoryImpl extends Repository<AuthorModel> implements Aut
     }
 
     @Override
-    public void create(AuthorModel model) {
-        throw new RuntimeException("Not implemented yet");
+    public void create(AuthorModel author) throws ConstraintViolationException {
+        try (var conn = DriverManager.getConnection(connectionString)){
+            String sql = "INSERT INTO " + getTableName() + " (name) VALUES (?)";
+            var statement = conn.prepareStatement(sql);
+            statement.setString(1, author.getName());
+            statement.execute();
+        } catch (SQLException e) {
+            Optional<SQLExceptionEnum> optionalExceptionType = SQLExceptionParser.parse(e);
+
+            boolean exceptionTypeNotFound = optionalExceptionType.isEmpty();
+            if (exceptionTypeNotFound) {
+                throw new RuntimeException(e);
+            }
+
+            switch (optionalExceptionType.get()) {
+                case CONSTRAINT_UNIQUE:
+                    String exceptionMessage = String.format("An author with this name (%s) already exists.", author.getName());
+                    throw new ConstraintViolationException(exceptionMessage);
+                case CONSTRAINT_NOTNULL:
+                    throw new ConstraintViolationException("A required field of the author is missing.");
+            }
+        }
+
+
     }
 
 }
--- src/main/java/com/esgi/domain/authors/AuthorMapper.java
+++ src/main/java/com/esgi/domain/authors/AuthorMapper.java
@@ -8,4 +8,10 @@ public class AuthorMapper {
                 authorModel.getName()
         );
     }
+    public AuthorModel entityToModel(AuthorEntity authorEntity){
+        return new AuthorModel(
+                authorEntity.getId(),
+                authorEntity.getName()
+        );
+    }
 }
