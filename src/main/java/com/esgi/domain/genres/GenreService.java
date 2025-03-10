2f371b295c269719ff04426699fd0d9b7be4a71f
AdrienDhmx <105926516+AdrienDhmx@users.noreply.github.com>
2025-03-10 22:14:00 +0100

    Fix: method conflicts

--- src/main/java/com/esgi/domain/genres/GenreService.java
+++ src/main/java/com/esgi/domain/genres/GenreService.java
@@ -7,5 +7,6 @@ public interface GenreService {
     GenreEntity getGenreById(int id) throws NotFoundException;
     void createGenre(GenreEntity genre) throws ConstraintViolationException;
     void updateGenre(GenreEntity genre) throws ConstraintViolationException,NotFoundException;
+    void deleteGenre(int id) throws NotFoundException, IllegalStateException, ConstraintViolationException;
     void deleteGenre(String name) throws NotFoundException,ConstraintViolationException;
 }
